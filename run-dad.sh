2f1e073becf2c0c959af2cd155548cc6ce420e41
okankop <okankopuklu@gmail.com>
2020-10-06 13:54:10 +0200

    gitignore added

--- run-dad.sh
+++ run-dad.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$PWD
 python main.py \
-  --root_path /usr/home/sut/datasets/DAD/DAD/ \
+  --root_path /usr/home/kop/datasets/DAD/ \
   --mode train \
   --view top_depth \
   --model_type resnet \
@@ -8,7 +8,7 @@ python main.py \
   --shortcut_type A \
   --pre_train_model False \
   --n_train_batch_size 10 \
-  --a_train_batch_size 150 \
+  --a_train_batch_size 140 \
   --val_batch_size 70\
   --learning_rate 0.01 \
   --epochs 250 \
